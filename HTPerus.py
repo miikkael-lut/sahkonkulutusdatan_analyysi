e15d701986daf1bb3fd1490d1c071170cd590be5
Miikkael-lut <miikkael.rantala@student.lut.fi>
2026-01-30 13:55:24 +0200

    Add files via upload

---
+++ HTPerus.py
@@ -0,0 +1,82 @@
+######################################################################
+# CT60A0203 Ohjelmoinnin perusteet
+# Tekijä: Miikkael Rantala
+# Päivämäärä: 7.11
+# Kurssin oppimateriaalien lisäksi työhön ovat vaikuttaneet seuraavat
+# lähteet ja henkilöt, ja se näkyy tehtävässä seuraavalla tavalla:
+# https://realpython.com ja ChatGPT funktioiden toiminnan kuvauksen tarkistus esim miten for toimii
+# Mahdollisen vilppiselvityksen varalta vakuutan, että olen tehnyt itse
+# tämän tehtävän ja vain yllä mainitut henkilöt sekä lähteet ovat
+# vaikuttaneet siihen yllä mainituilla tavoilla.
+######################################################################
+import HTPerusKirjasto
+
+def valikko(): 
+    while True:
+        print("Valitse haluamasi toiminto:")
+        print("1) Lue tiedosto")
+        print("2) Analysoi")
+        print("3) Kirjoita tiedosto")
+        print("4) Analysoi viikonpäivittäiset tulokset")
+        print("0) Lopeta")
+        
+        Valinta = int(input("Anna valintasi: "))
+        if Valinta in [0, 1, 2, 3, 4]:
+            return Valinta
+        else:
+            print("Tuntematon valinta, yritä uudestaan.")
+            print()
+
+    return None        
+def paaohjelma():
+    Valinta=None
+    Tiedot=[]
+    Rivi=[]
+    Luettava_tiedosto=None
+    Analyysitehty=None
+    Paivakustannukset=[]
+    Kuukausikustannukset=[] 
+    while Valinta != 0:
+        Valinta=valikko()
+        if (Valinta==1):
+            Luettava_tiedosto=HTPerusKirjasto.tiedosto(Valinta)
+            Tiedot=HTPerusKirjasto.kysyjalue(Tiedot, Luettava_tiedosto)
+                  
+        elif (Valinta==2):
+            if len(Tiedot) == 0:
+                print("Ei tietoja analysoitavaksi, lue tiedot ennen analyysiä.")
+            else:
+                Paivakustannukset.clear()
+                Kuukausikustannukset.clear()
+                Kuukausikustannukset=HTPerusKirjasto.lueJaAnalysoi(Tiedot, Kuukausikustannukset)
+                Analyysitehty=1
+                  
+        elif (Valinta==3):
+            if Analyysitehty !=1 :
+                print("Ei tietoja tallennettavaksi, analysoi tiedot ennen tallennusta.")
+            else:    
+                Luettava_tiedosto=HTPerusKirjasto.tiedosto(Valinta)
+                HTPerusKirjasto.tallenna(Luettava_tiedosto, Paivakustannukset, Kuukausikustannukset)
+                
+        elif (Valinta==4):
+            if len(Tiedot) == 0:
+                print("Ei tietoja analysoitavaksi, lue tiedot ennen analyysiä.")
+            else:
+                Paivakustannukset.clear()
+                Kuukausikustannukset.clear()                
+                Paivakustannukset=HTPerusKirjasto.luejaAnalysoi2(Tiedot, Paivakustannukset)              
+                Luettava_tiedosto=HTPerusKirjasto.tiedosto(Valinta)
+                HTPerusKirjasto.tallenna(Luettava_tiedosto, Paivakustannukset)
+        elif (Valinta ==0):
+            print("Lopetetaan.")
+        print()
+    Tiedot.clear()
+    Paivakustannukset.clear()
+    Kuukausikustannukset.clear()
+    # Rivi.clear()
+    print("Kiitos ohjelman käytöstä.")
+
+    return None
+
+paaohjelma()
+
---
+++ HTPerusKirjasto.py
@@ -0,0 +1,128 @@
+######################################################################
+# CT60A0203 Ohjelmoinnin perusteet
+# Tekijä: Miikkael Rantala
+# Päivämäärä: 30.11
+# Kurssin oppimateriaalien lisäksi työhön ovat vaikuttaneet seuraavat
+# lähteet ja henkilöt, ja se näkyy tehtävässä seuraavalla tavalla:
+# https://realpython.com ja ChatGPT funktioiden toiminnan kuvauksen tarkistus esim miten for tai write toimii
+# Mahdollisen vilppiselvityksen varalta vakuutan, että olen tehnyt itse
+# tämän tehtävän ja vain yllä mainitut henkilöt sekä lähteet ovat
+# vaikuttaneet siihen yllä mainituilla tavoilla.
+######################################################################
+import time
+from datetime import datetime
+import sys
+class KULUTUKSET:
+    Aika=None
+    Kulutus=float(0)
+    KulutusPaiva=float(0)
+    KulutusYo=float(0)   
+    
+# class RIVIT:                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+    # KulutusMaara=float(0)
+    # Kulutuskeski=int(0)
+    # MatalinKulutus=0
+    # KorkeinKulutus=0
+    # MatalaAika=None
+    # KorkeaAika=None
+
+   
+def tiedosto(Valinta):
+    if Valinta ==1:
+        Luettava_tiedosto=input("Anna luettavan tiedoston nimi: ")     
+
+    elif Valinta ==3 or 4:
+        Luettava_tiedosto=input("Anna kirjoitettavan tiedoston nimi: ")
+         
+    return Luettava_tiedosto
+    
+def kysyjalue(Tiedot, Luettava_tiedosto):
+    try:
+        Tiedosto=open(Luettava_tiedosto, "r", encoding="utf-8")
+        Rivi = Tiedosto.readline()
+        Tiedot.clear()
+        Askel=0
+        for Rivi in Tiedosto:
+            Tieto = KULUTUKSET()
+            Sarakkeet = Rivi.strip().split(";")
+            Tieto.Aika = Sarakkeet[0]
+            Tieto.KulutusYo=float(Sarakkeet[1])
+            Tieto.KulutusPaiva=float(Sarakkeet[2])
+            Tieto.Kulutus=Tieto.KulutusPaiva+Tieto.KulutusYo
+            Tiedot.append(Tieto)
+            Askel +=1
+        Tiedosto.close()
+        print("Tiedostosta '"+Luettava_tiedosto+"' lisättiin listaan", Askel, "datariviä.")  
+    except OSError:
+        print("Tiedoston '", Luettava_tiedosto, "' käsittelyssä virhe, lopetetaan.", end="", sep="")
+        sys.exit()
+    return Tiedot
+    
+def lueJaAnalysoi(Tiedot,Kuukausikustannukset):
+    # Rivi= RIVIT()
+    EdellinenKuukausi = None
+    KuukausikulutusPaiva = float(0)
+    KuukausikulutusYo = float(0)
+    KuukausienMaara=1
+
+    for Tieto in Tiedot:
+        
+        Aika = time.strptime(Tieto.Aika, "%d-%m-%Y %H:%M")
+        AikaKuukausi = Aika.tm_mon
+        
+        if AikaKuukausi != EdellinenKuukausi and EdellinenKuukausi is not None:
+            Kuukausikustannukset.append((EdellinenKuukausi, KuukausikulutusPaiva, KuukausikulutusYo))
+            KuukausikulutusPaiva = 0.0
+            KuukausikulutusYo = 0.0
+            KuukausienMaara += 1
+
+        KuukausikulutusPaiva += Tieto.KulutusPaiva
+        KuukausikulutusYo += Tieto.KulutusYo
+        EdellinenKuukausi = AikaKuukausi
+    
+    Kuukausikustannukset.append((EdellinenKuukausi, KuukausikulutusPaiva, KuukausikulutusYo))
+  
+    print("Kuukausittaiset summat laskettu", KuukausienMaara ,"kuukaudelle.")
+    return Kuukausikustannukset
+    
+
+    
+def luejaAnalysoi2(Tiedot,Paivakustannukset):  
+    ViikonpaivaKulutukset = [0.0] * 7
+            
+    for Tieto in Tiedot:
+        Aika = time.strptime(Tieto.Aika, "%d-%m-%Y %H:%M")
+        AikaPaiva = Aika.tm_wday  
+        ViikonpaivaKulutukset[AikaPaiva] += Tieto.Kulutus
+
+    Paivakustannukset = [(i, ViikonpaivaKulutukset[i]) for i in range(7)]
+             
+    return Paivakustannukset
+
+    
+def tallenna(Luettava_tiedosto, Paivakustannukset,Kuukausikustannukset):
+    try:
+        Tiedosto = open(Luettava_tiedosto, "w", encoding="utf-8")
+        
+        if Kuukausikustannukset:
+            Tiedosto.write("Kuukausittaiset kulutukset (MWh):\n")
+            Tiedosto.write("Kuukausi;Yö;Päivä;Yhteensä\n")
+            for EdellinenKuukausi, KuukausikulutusPaiva, KuukausikulutusYo in Kuukausikustannukset:
+                Tiedosto.write(datetime(1900, EdellinenKuukausi, 1).strftime("%b")
+                + ";" 
+                + str(round(KuukausikulutusYo/1000,1)) +";"
+                + str(round(KuukausikulutusPaiva/1000,1)) +";" 
+                + str(round((KuukausikulutusYo+KuukausikulutusPaiva)/1000,1)) + "\n")
+            Tiedosto.write("\n")
+                
+        if Paivakustannukset:
+            Tiedosto.write("Viikonpäivä;Kulutus (MWh)\n")
+            for Paiva, Kulutus in Paivakustannukset:
+                Tiedosto.write(["Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai", "Sunnuntai"][Paiva] + ";" + str(round(Kulutus/1000,1))+ "\n")
+                
+        print("Tiedosto '"+Luettava_tiedosto+"' kirjoitettu.") 
+        Tiedosto.close()
+    except OSError:
+        print("Tiedoston '", Luettava_tiedosto, "' käsittelyssä virhe, lopetetaan.", end="", sep="")
+        sys.exit()
+    return None
